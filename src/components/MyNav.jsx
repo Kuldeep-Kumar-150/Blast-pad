6de6b87a1389db7c34d2a0c6cdaba4c5a4a1901b
Kuldeep-Kumar-150 <swamikuldeep654@gmail.com>
2023-05-22 10:16:26 +0530

    first commit

---
+++ src/components/MyNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MyNav = () => {
+    return (
+        <section>
+            <div className="container mx-auto px-3">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, praesentium?</p>
+            </div>
+        </section>
+    )
+}
+
+export default MyNav
